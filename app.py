1a05c90fad30c298687dbedb1a350ce06e7c5dc6
hanielrolemberg <victorhanielbusiness@gmail.com>
2024-06-02 01:05:47 -0300

    seleção de contatos

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+#importar tbm o keys - para poder mandar um crtl, backspace, shift etc
+from selenium.webdriver.common.keys import Keys
+
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
@@ -20,3 +23,20 @@ barra_lateral = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="s
 #Uma vez ele carregado, entra o Momento do mapeamento da barra lateral (onde constam os contatos/msg/etc)
 
 barra_lateral = driver.find_element(by=By.XPATH, value='//*[@id="side"]')
+
+#Adicionando uma espera global
+driver.implicitly_wait(2)
+
+#Momento de abrir janela de conversa
+
+
+barra_pesquisa = driver.find_element(By.XPATH, '//*[@id="side"]/div[1]/div/div[2]/div[2]/div/div[1]')   #se esse xpath não funcionar, vá neste '//div[@title="Caixa de texto de pesquisa"]'
+
+#permite limpar o campo de inserção de novo usuário
+barra_pesquisa.send_keys(Keys.CONTROL + 'a')
+barra_pesquisa.send_keys(Keys.DELETE)
+
+#agora é feita a pesquisa em si do contato
+nome_contato = "Cliente1"
+barra_pesquisa = driver.find_element(By.XPATH, '//*[@id="side"]/div[1]/div/div[2]/div[2]/div/div[1]')   #se esse xpath não funcionar, vá neste '//div[@title="Caixa de texto de pesquisa"]'
+barra_pesquisa.send_keys(nome_contato) #key de keyboard, isto é, teclado
